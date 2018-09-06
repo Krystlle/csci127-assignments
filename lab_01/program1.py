8a19958429bbffe8d30b411e01c8db41ee7440ea
krystlle <fajardokrystlle@gmail.com>
2018-09-06 10:22:51 -0400

    added string to program 1

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -7,3 +7,13 @@ print(greeter("Ollie"));
 def make_tags(tag,word)
     result="<"+tag+">"+word+"<"+tag+">"
     return result
+
+def make_out_word(out,word):
+    result=out[:2]+word+[out][2:]
+    return result
+
+#looked up how to halve syntax or 'out' symbols, python syntax splicing:
+#a[start:end] # items start through end-1
+#a[start:]    # items start through the rest of the array
+#a[:end]      # items from the beginning through end-1
+#a[:]         # a copy of the whole array
