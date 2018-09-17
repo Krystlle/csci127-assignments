2d6b4c2674a9772d3412f7a62fbe70d412cca6fb
krystlle <fajardokrystlle@gmail.com>
2018-09-17 00:09:38 -0400

    updating hw

--- program.py
+++ program.py
@@ -9,6 +9,8 @@ def capitalize_name(name):
     return (first_last)
 
 print(capitalize_name("krystlle fajardo"))
+#success
+
 #2
 def init(name):
     """
@@ -16,14 +18,16 @@ def init(name):
     Returns: a string in the form "F. Last" where it's a capitalized first inital 
     and capitalized last name
     """
-    s= name.title()
+    s=name.title()
     first=s[0]
     last=s.split()
-    first_last=first + last
-    return first_last
+    f_l=first + last
+    return f_l
 
 print(init("krystlle fajardo"))
-#couldn't figure ot how to get last name to show up, was able to get intial only
+#couldn't figure out how to get last name to show up, was able to get intial only, the last=s.split() part seems to detect return as a list
+#and will not print anything b/c of the 'last' component. if it was only first, it would print successfully
+
 
 #3
 
@@ -33,12 +37,25 @@ def part_pig_latin(name):
     Returns: that string in fake pig latin -> move the first letter of the word to the end and add "ay"
     so: "hello" --> "ellohay"
     """
+    s=name[1:]+name[0]
+    pig_word=s + "ay"
+    return pig_word
+
+print(part_pig_latin("krystlle"))
+#success
+
 # 4-5 from codingbat 
 
 def make_tags(tag,word):
     result="<"+tag+">"+word+"<"+tag+">"
     return result
+print(make_tags("i","oof"))
+
+#success
 
 def make_out_word(out,word):
-    result=out[:2]+word+[out][2:]
+    result=out[:2] + word + [out][2:]
     return result
+print(make_out_word("oh","okay"))
+
+#works on codingbat, not on here?
