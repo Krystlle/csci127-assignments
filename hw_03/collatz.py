039343ad234d3849e033c581bca4366b98bfeb45
krystlle <fajardokrystlle@gmail.com>
2018-09-27 09:01:29 -0400

    updating collatz

--- hw_03/collatz.py
+++ hw_03/collatz.py
@@ -1,15 +1,14 @@
 def seq(n):
     count = 0
-   while (n =! 1:)
-        if (n % 2==0)
+    while (n != 1):
+        if (n % 2==0):
            n== n // 2
            count+=1
-        else
+        else:
             n== n * 3 +1
             count+=1
             print(n)
-        return "this is the nuber of steps" + sequence(count)
-    
-#With Madison Chen, Nursima Donuk 
-        
+        return "this is the number of steps" + str(count)
+print(seq(10))
+#Krystlle Fajardo with Madison Chen, Nursima Donuk 
             
