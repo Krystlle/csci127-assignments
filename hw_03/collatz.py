6ff1060080d656c9f7c5b12b259991fcd9202411
krystlle <fajardokrystlle@gmail.com>
2018-09-27 08:50:46 -0400

    adding assignment

---
+++ hw_03/collatz.py
@@ -0,0 +1,15 @@
+def seq(n):
+    count = 0
+   while (n =! 1:)
+        if (n % 2==0)
+           n== n // 2
+           count+=1
+        else
+            n== n * 3 +1
+            count+=1
+            print(n)
+        return "this is the nuber of steps" + sequence(count)
+    
+#With Madison Chen, Nursima Donuk 
+        
+            
