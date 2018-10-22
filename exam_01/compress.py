3030c265418f34fe32d368c8b7474da9b084633c
krystlle <fajardokrystlle@gmail.com>
2018-10-22 09:36:41 -0400

    changed compress.py

--- exam_01/compress.py
+++ exam_01/compress.py
@@ -1,10 +1,12 @@
-
+words = ["Halloween","Special","apple"] 
 def compress_word(w):
     vowels = ["a","e","i","o","u","A","E","I","O","U"]
- 
-    for w in len(s):
+    vowels = str(["a","e","i","o","u","A","E","I","O","U"])
+    for w in words:
         if vowels in w:
-            sans_vowels = w - vowels
+            print(w,len(w))
+            select = len(w).replace(" ")
+            sans_vowels[select] = w - vowels
             print(sans_vowels)
         else:
             return sans_vowels
