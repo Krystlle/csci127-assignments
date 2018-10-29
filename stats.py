93d1f48a39fe2339fecab065b91703a22e0dccc8
krystlle <fajardokrystlle@gmail.com>
2018-10-29 01:08:25 -0400

    updated hw_06

--- stats.py
+++ stats.py
@@ -1,14 +1,38 @@
-import random
+from random import randrange
 
-""" 100 tems, value 0-10, find the location of  one of the largest, then the frequency of a specific value, for (2,7) e-> # of times appears"""
-
-list = []
-highest = max(list)
-
-largest = list[0]
-for i in range (0,10):
-    list.append(random.randrange(0,11,1))
-    if i == highest : 
-        list.index(max(values))
+l = []
+def build_list(items,max_value):
+    i = 0
+    for i in range (100):
+        l.append(randrange(1,100))
+    return l
+print(l)
     
-print (max(values))
+
+def index_largest(l):
+    li = 0
+    for i in range(1,len(l)):
+        if l[i] > l [i]:
+            li = i
+    return li
+
+def freq(l,value):
+    freq_list= []
+    for i in l:
+        count = freq_list.count(max(items))
+        count.append()
+    return count
+
+#doesn't work :
+def mode(l):
+    for i in range(len(l)):
+        if i in l:
+           count += 1
+        else:
+           count = 1
+    return 
+
+li = index_largest(l)
+print(build_list(li,l[li]))
+print(freq(count))
+print(mode(l))
