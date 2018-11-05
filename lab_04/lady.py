082e89b6b3acb36cc7ebd9f0402b5f18a608ebe9
krystlle <fajardokrystlle@gmail.com>
2018-11-04 22:40:34 -0500

    added lady.py

---
+++ lab_04/lady.py
@@ -0,0 +1,18 @@
+isEmpty = False    
+isHappy = True
+
+def happyLadybugs(n,b):
+
+    for i in range ():
+        if i != "_" #blank 
+        enumerate()
+    
+    return "YES"
+
+    return "NO"
+
+b = input()
+n = 
+print(happyLadybugs(n,b))
+
+
