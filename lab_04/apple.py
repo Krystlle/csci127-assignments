4d66e7a76d783151eaca18b9e9bdce307b3e6fbf
krystlle <fajardokrystlle@gmail.com>
2018-11-04 20:15:44 -0500

    added lab_04

---
+++ lab_04/apple.py
@@ -0,0 +1,17 @@
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    
+    countApple = 0
+    countOrange = 0
+
+    for x in apples:
+        if  (x + a) >= s and (x + b) <= t
+            countApple += 1
+    
+    for x in oranges:
+        if (x + a) >= s and (x + b) <= t
+            countOrange += 1 
+
+#int(input())
+
+print(countApple)
+print(countOrange)
