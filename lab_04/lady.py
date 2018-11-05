e23bf134793232f6a10a6585a0ec0a10938bcbec
krystlle <fajardokrystlle@gmail.com>
2018-11-04 23:43:54 -0500

    pdated lady.py

--- lab_04/lady.py
+++ lab_04/lady.py
@@ -1,18 +1,24 @@
-isEmpty = False    
-isHappy = True
 
 def happyLadybugs(n,b):
 
-    for i in range ():
-        if i != "_" #blank 
-        enumerate()
-    
-    return "YES"
-
-    return "NO"
-
+    for i in b:
+        if i != "_" :
+            return "NO"
+        elif n == 0:
+            return "NO"
+        elif len(b) < 2:
+            return "NO"
+        elif b[0] == b[1]:
+            return "YES"
+            
 b = input()
-n = 
 print(happyLadybugs(n,b))
 
+#issue with 'return' indentation to getting count to work 
+#count = [] 
+
+#for i in b:
+ #   if i is "YES":
+  #      count = counter + 1 
+#return count
 
