b4ebf09c157edc434bb935f21e86da39a2122a0c
krystlle <fajardokrystlle@gmail.com>
2018-11-08 01:11:11 -0500

    edited lady.py

--- lab_04/lady.py
+++ lab_04/lady.py
@@ -1,6 +1,7 @@
 
 def happyLadybugs(n,b):
 
+
     for i in b:
         if i != "_" :
             return "NO"
@@ -10,6 +11,21 @@ def happyLadybugs(n,b):
             return "NO"
         elif b[0] == b[1]:
             return "YES"
+    for i in range(0,(len(b) - 1)):
+        if b[i] != b[i-1] and b[i] != [i+1]: #check if the letters match the ones beside each other 
+            return "NO"
+        else:
+            return "YES"
+        
+def count_bugs(b):
+    bugs = {}
+    for i in b:
+        if i in b: 
+            bugs[i] = 0 
+            bugs[i] = bugs[i] + 1
+        else:
+            bugs[i] = bugs[i] + 0 
+return bugs
             
 b = input()
 print(happyLadybugs(n,b))
