c734f4338587557ff5087437bf0f697a171fb8f5
krystlle <fajardokrystlle@gmail.com>
2018-11-19 09:26:20 -0500

    edited rle.py

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -5,16 +5,15 @@ d2 = {}
 def encode(w):
     string = d.keys()
     s = string.split("") #split key phrase into characters, e.g. 'abc' is 'a', 'b', 'c'
-    [i[0] for word in string]
-        d.setdefault(s[i],[])
-        d[s[i]].append(s[i+1])
+    s.count()
+    d.setdefault(s[i],0) #set to 0 for counting
+    d[s[i]].append(s[i+1])
     return d        
 
-def count_letters(w):
-    d={}
-    for i in string.split():
-        d.setdefault(string,0)
-        d[string]=d[string]+1
-    return d
+#def count_letters(w):
+ #   for i in string.split():
+  #      d.setdefault(string,0)
+   #     d[string]=d[string]+1
+    #return d
 
-print(encode(count_letters))
+print(encode)
