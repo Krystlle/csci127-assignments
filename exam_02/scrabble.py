f707cf3c0f45de0383579594edf590f6613b9668
krystlle <fajardokrystlle@gmail.com>
2018-11-19 09:35:01 -0500

    added scrabble.py

---
+++ exam_02/scrabble.py
@@ -0,0 +1,6 @@
+def scrabble(w):
+    d = {"AEIOULNRST": 1, "DG": 2, "BCMP": 3, "FHVWY": 4, "K": 5, : "JK" : 8, "QZ": 10}
+    scoring = 0
+    for i in w:
+        total = total+d[.lower()]
+    return total
