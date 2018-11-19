edee4996189258e7795e0ff95be3277206b47233
krystlle <fajardokrystlle@gmail.com>
2018-11-19 09:38:05 -0500

    Edited scrabble

--- exam_02/scrabble.py
+++ exam_02/scrabble.py
@@ -2,5 +2,5 @@ def scrabble(w):
     d = {"AEIOULNRST": 1, "DG": 2, "BCMP": 3, "FHVWY": 4, "K": 5, : "JK" : 8, "QZ": 10}
     scoring = 0
     for i in w:
-        total = total+d[.lower()]
+        scoring = scoring+d[]
     return scoring
