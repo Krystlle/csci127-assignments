79bad990bb45c44c43f81684e0ee56874e904a04
krystlle <fajardokrystlle@gmail.com>
2018-12-03 00:52:33 -0500

    added hw_09

---
+++ hw_09/count.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+  i = 1;
+  for(int i : 20){
+  std::cout << "i = " << i << std::endl;
+  i = i + 1
+  }
+}
---
+++ hw_09/guess.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+  int input;
+  int calc;
+  int guess;
+  int min = 0 ;
+  int max = 99;
+
+  stdd::cout <<"Enter a number from 0-99"
+  stdd::cin >> input << stdd::endl;
+  stdd::cout << "Guess: " << guess << stdd::endl;
+  stdd::cin >> guess;
+  stdd::cout << "If Higher = 1 Lower = -1 Correct = 0 " << stdd::endl;
+  stdd::cin >> calc;
+}
+  while (int calc != 0) {
+    guess = stdd::std::cout << "If Higher = 1 Lower = -1 Correct = 0" << '\n';
+    if (int calc == -1 )
+      guess;
+  }
+    else {
+      guess;
+    }
+  return 0
+  0 = std::cout << "Correct!" << stdd::endl;;
+}
