fbdbfbeb6232755b8b814159c6d9192694211739
krystlle <fajardokrystlle@gmail.com>
2018-12-10 09:22:08 -0500

    edited sum.cpp

--- final_1/sum.cpp
+++ final_1/sum.cpp
@@ -1,9 +1,21 @@
 // question 2
 #include <iostream>
+#include <math.h>
+
 int sumofsquare(int a, int b){
-  a = 2;
-  b = 4;
-  for (size_t i = 0; i < count; i++) {
-    /* code */
+  int a = 4;
+  int a2;
+  int b = 8;
+  int answer;
+  for (int a = 4; a < b; a++) {
+    a2 = a * 2;
+    if (a < b) {
+      answer = a2 + a2
+    }
+  return answer;
   }
+
+int int main() {
+  std:cout << sumofsquare(4,8) << '\n';
+  return 0;
 }
