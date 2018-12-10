3e467f905986b89a1f4db1512b61b68d766dbe51
krystlle <fajardokrystlle@gmail.com>
2018-12-10 09:24:15 -0500

    updated sum.cpp

--- final_1/sum.cpp
+++ final_1/sum.cpp
@@ -7,11 +7,15 @@ int sumofsquare(int a, int b){
   int a2;
   int b = 8;
   int answer;
-  for (int a = 4; a < b; a++) {
+  for (int a = 4; a >= b; a++) {
     a2 = a * 2;
     if (a < b) {
-      answer = a2 + a2
+      answer = a2 + a2;
     }
+  else(a < b); {
+    answer = a2 + 0;
+  }
+
   return answer;
   }
 
