5ac88916ee1d67a6839965a760ad90e7da368e6c
krystlle <fajardokrystlle@gmail.com>
2018-12-20 10:56:54 -0500

    Adding readme.md

--- final_2/dict.py
+++ final_2/dict.py
@@ -1,8 +1,27 @@
 def addLine(d,line):
     d = {}
     line = line.lower()
+    words = line.split("")
+    print(words)
+    theRange = words.range(0, len(words))
     print(line)
+    for letter in theRange:
+        if letter in words[0] == words[:+1]
+        d[words[letter]]
+        
+        d[word] = d[word] + 1
     d.setdefault(0,line)
+    
+def
+
+#3.3
+def spellcheck(d,word):
+    word = word.lower()
+        if word is in d:
+            return True
+        else:
+            return False
+
     
 print(addLine(d, "Four for you, Glenn Coco! You go, Glenn Coco!"))
 print(addLine(d,"I will travel across the land, searching far and wide. Each Pokemon to understand the power that's inside!"))
